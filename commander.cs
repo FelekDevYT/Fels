b6f7f261ce93f5328f244f3155f11a99583d2839
FelekDevYT <157293851+FelekDevYT@users.noreply.github.com>
2024-02-17 11:28:23 +0300

    Update commander.cs

--- commander.cs
+++ commander.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Xml.Schema;
+using langg;
 
 namespace lang
 {
@@ -12,14 +14,67 @@ namespace lang
         public static void MAIN(String path)
         {
             String[] str = File.ReadAllLines(path);
+            String[] var_value = new String[1024];
+            String[] var_names = new String[1024];
+            int uses = 0;
+            int zc = 0;
+            int use = 0;
             for (int i = 0; i < str.Length; i++)
             {
-                String[] line = str[i].Split(' ');//writeln[0] "11"[1]
+                String[] line = str[i].Split(' ');
                 switch (line[0])
                 {
                     case "writeln":
-                        String[] l = str[i].Split('"','"');
-                        Console.WriteLine(l[1]);
+                        String[] l = str[i].Split('"', '"');
+                        if(line.Length == 4)
+                        {
+                            Console.WriteLine(Parser.NumericExpression(Convert.ToInt32(line[1]), line[2], Convert.ToInt32(line[3])));
+                        }
+                        if(l.Length > 1)
+                        {
+                            Console.WriteLine(l[1]);
+                        }
+                        else
+                        {
+                            use = 0;
+                            foreach(String el in var_names)
+                            {
+                                String g;
+                                g = line[1];
+                                if (el == g)
+                                {
+                                    Console.WriteLine(var_value[use]);
+                                    break;
+                                }
+                                use++;
+                            }
+                        }
+                        break;
+                    case "write":
+                        String[] ln = str[i].Split('"', '"');
+                        if (line.Length == 4)
+                        {
+                            Console.Write(Parser.NumericExpression(Convert.ToInt32(line[1]), line[2], Convert.ToInt32(line[3])));
+                        }
+                        if (ln.Length > 1)
+                        {
+                            Console.Write(ln[1]);
+                        }
+                        else
+                        {
+                            use = 0;
+                            foreach (String el in var_names)
+                            {
+                                String g;
+                                g = line[1];
+                                if (el == g)
+                                {
+                                    Console.Write(var_value[use]);
+                                    break;
+                                }
+                                use++;
+                            }
+                        }
                         break;
                     case "color":
                         if (line[1] == "red") Console.ForegroundColor = ConsoleColor.Red;
@@ -30,6 +85,35 @@ namespace lang
                     case "readKey":
                         Console.ReadKey();
                         Console.WriteLine();
+                        break;
+                    case "var":
+                        String[] eq = str[i].Split('=');
+                        var_value[uses] = eq[1];
+                        var_names[uses] = line[1];
+                        break;
+                    case "read":
+                        uses = 0;
+                        zc = 0;
+                        foreach(String el in var_names)
+                        {
+                            String gl = line[1];
+                            if(gl == el)
+                            {
+                                var_value[use] = Console.ReadLine();
+                                use = 0;
+                                zc = 0;
+                                break;
+                            }
+                            zc++;
+                            use++;
+                        }
+                        if(zc == 0){
+                            break;
+                        }
+                        else
+                        {
+                            Exceptions.NonVarException();
+                        }
                         break;
                 }
             }
