3eb978312aa472878ffdf173ee92c4fa967fce7c
FelekDevYT <157293851+FelekDevYT@users.noreply.github.com>
2024-02-17 11:29:18 +0300

    Create parser and exceptions class

---
+++ parser.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace langg
+{
+    public class Parser
+    {
+        public static int NumericExpression(int st1,String el,int st2)
+        {
+            switch (el)
+            {
+                case "+":
+                    return st1 + st2;
+                case "-":
+                    return st1 - st2;
+                case "*":
+                    return st1 * st2;
+                case "/":
+                    return st1 / st2;
+                case "%":
+                    return st1 % st2;
+                case "**":
+                    return (int) Math.Pow(st1,st2);
+            }
+            return 0;
+        }
+    }
+    public class Exceptions
+    {
+        public static void OperathorNotFound() { Console.WriteLine("Operathor not found!"); Console.ReadKey(); Environment.Exit(-111); }
+        public static void NonVarException() { Console.WriteLine("Variable not found!");Console.ReadKey();Environment.Exit(-112); }
+        public static void WritelnException() { Console.WriteLine("Variable or String not found!");Console.ReadKey();Environment.Exit(-113); }
+    }
+}
